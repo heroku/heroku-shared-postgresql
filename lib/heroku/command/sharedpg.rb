8783ea0d15b9841897a365efc12cd053eae2758b
Curt Micol <asenchi@asenchi.com>
2011-09-26 17:00:57 -0400

    Try to get a new parse_config in.
    My experience with Ruby and (private, public, protected) is limited. So lets try
    and monkey patch in a parse_config that notices our _URL.

--- lib/heroku/command/sharedpg.rb
+++ lib/heroku/command/sharedpg.rb
@@ -1,6 +1,5 @@
 require "heroku/command/base"
 require "heroku/pgutils"
-require "heroku/pg_resolver"
 require "heroku/shared_pg_resolver"
 require "heroku-postgresql/client"
 
--- lib/heroku/shared_pg_resolver.rb
+++ lib/heroku/shared_pg_resolver.rb
@@ -1,7 +1,30 @@
+require "heroku/pg_resolver"
+
 module PGResolver
-  class SharedResolver
+  class SharedResolver < Resolver
+    include PGResolver
+
     def self.shared_addon_prefix
       ENV["HEROKU_SHARED_DATABASE_ADDON_PREFIX"] || "HEROKU_SHARED_POSTGRESQL"
+    end
+
+    private
+
+    def self.parse_config(config_vars)
+      dbs = {}
+      config_vars.each do |key,val|
+        case key
+        when "DATABASE_URL"
+          dbs['DATABASE'] = val
+        when 'SHARED_DATABASE_URL'
+          dbs['SHARED_DATABASE'] = val
+        when /^(#{shared_addon_prefix}\w+)_URL$/
+          dbs["HEROKU_SHARED_POSTGRESQL"] = val
+        when /^(#{addon_prefix}\w+)_URL$/
+          dbs[$+] = val # $+ is the last match
+        end
+      end
+      return dbs
     end
   end
 end
